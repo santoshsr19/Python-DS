e08e5e523383a64ce5f698a21c14d657a4027ed1
G2-India <santosh.sr19@gmail.com>
2020-09-25 14:57:24 +0530

    Nuts and bolts problem solved

---
+++ Array/nutsandbolts.py
@@ -0,0 +1,38 @@
+class Solution:
+
+	def matchPairs(self,nuts, bolts, n):
+	    nuts.sort()
+	    bolts.sort()
+	    return nuts, bolts
+		 
+	    
+
+
+
+
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(nuts, bolts, n)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
