bbd6c9e97ca032953657a4b31bff6e0f1138a157
santoshsr <santosh.sr19@gmail.com>
2020-09-28 16:13:40 +0530

    Practiced Geeks for geeks array prog

--- Array/arraysort.py
+++ Array/arraysort.py
@@ -18,3 +18,48 @@ if __name__ == '__main__':
         print()
 
 # } Driver Code Ends
+
+
+#repeated problem in geeks for geeks
+
+def nearlysorted (arr, n, k):
+    arr.sort()
+    for i in arr:
+        print(i,end=" ")
+    print()
+    
+    
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    arr=[int(x) for x in input().strip().split()]
+    nearlysorted (arr, n, k)
+
+"""
+
+Given an array of n elements, where each element is at most k away from its target position. The task is to print array in sorted form.
+
+Input:
+First line consists of T test cases. First line of every test case consists of two integers N and K, denoting number of elements in array and at most k positions away from its target position respectively. Second line of every test case consists of elements of array.
+
+Output:
+Single line output to print the sorted array.
+
+Constraints:
+1<=T<=100
+1<=N<=100
+1<=K<=N
+
+Example:
+Input:
+2
+3 3
+2 1 3
+6 3
+2 6 3 12 56 8
+Output:
+1 2 3
+2 3 6 8 12 56
+
+
+"""
---
+++ Array/mergesortedarray.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+def merge(numbers):
+    a=[]
+    for i in numbers:
+        for k in i:
+            a.append(k)
+    a.sort()
+    return a
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        numbers=[[ 0 for _ in range(n) ] for _ in range(n) ]
+        line=input().strip().split()
+        for i in range(n):
+            for j in range(n):
+                numbers[i][j]=int(line[i*n+j])
+        merged_list=merge(numbers)
+        for i in merged_list:
+            print(i,end=' ')
+        print()
+# } Driver Code Ends
+
+
+"""
+Input:
+K = 3
+arr[][] = {{1,2,3},{4,5,6},{7,8,9}}
+Output: 1 2 3 4 5 6 7 8 9
+Explanation:Above test case has 3 sorted
+arrays of size 3, 3, 3
+arr[][] = [[1, 2, 3],[4, 5, 6], 
+[7, 8, 9]]
+The merged list will be 
+[1, 2, 3, 4, 5, 6, 7, 8, 9].
+"""
