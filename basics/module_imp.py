f0639b9ccc1b3b2674b560ffefaf4a355c335267
santoshsr <santosh.sr19@gmail.com>
2020-09-29 21:18:04 +0530

    Practiced and solved basic python programs from programiz website

---
+++ basics/module_imp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 12 21:01:08 2018
+
+@author: Ashusan
+"""
+
+import my_module
+dir(my_module)
+my_module._name_
+
+imp.reload(my_module)
+
+a=8
+b='hello'
+import math
+dir()
