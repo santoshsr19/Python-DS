444bec26fb33d64fc3835b0b12e35b1d5fd3baca
santoshsr <santosh.sr19@gmail.com>
2020-11-08 21:02:33 +0530

    Wrote oops program for linked list

---
+++ LinkedList/Linkedlist.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def inset_at_begining(self, data):
+        node  = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + '--->'
+            itr = itr.next
+
+        print(llstr)
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+    def remove_at(self, index):
+        if index<0 or index>self.get_length()
+            raise Exception("Invalid index")
+
+        if index == 0:
+            self.head =s self.head.next
+            return
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(['banana', 'apple', 'mango', 'bracolli'])
+    ll.print()
+    print("length",ll.get_length())
